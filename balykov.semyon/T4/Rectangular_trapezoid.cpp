b578ee644150181785f7b8acf551108cdd52d944
Balykov Semyon <balykov.semen2010@mail.ru>
2026-03-10 13:51:35 +0300

    initial for T4

---
+++ balykov.semyon/T4/Rectangular_trapezoid.cpp
@@ -0,0 +1,37 @@
+#include "rectangular_trapezoid.h"
+#include <iostream>
+#include <string>
+Rectangular_trapezoid::Rectangular_trapezoid(const Point& bl, double lowerBase, double upperBase, double height) : bottomLeft(bl), lowerBase_(lowerBase), upperBase_(upperBase), height_(height) {}
+
+double Rectangular_trapezoid::getArea() const {
+    return ((lowerBase_ + upperBase_) / 2.0) * height_;
+}
+Point Rectangular_trapezoid::getCenter() const {
+    double leftMidX = bottomLeft.x;
+    double leftMidY = bottomLeft.y + (height_ / 2.0);
+    double rightMidX = (bottomLeft.x + lowerBase_ + bottomLeft.x + upperBase_) / 2.0;
+
+    double centerX = (leftMidX + rightMidX) / 2.0;
+    double centerY = leftMidY;
+
+    return Point(centerX, centerY);
+}
+void Rectangular_trapezoid::move(double dx, double dy) {
+    bottomLeft.x += dx;
+    bottomLeft.y += dy;
+}
+void Rectangular_trapezoid::scale(double factor) {
+    lowerBase_ *= factor;
+    upperBase_ *= factor;
+    height_ *= factor;
+    Point center = getCenter();
+    bottomLeft.x = center.x + (bottomLeft.x - center.x) * factor;
+    bottomLeft.y = center.y + (bottomLeft.y - center.y) * factor;
+}
+
+std::string Rectangular_trapezoid::getName() const {
+    return "RECTANGULAR TRAPEZOID";
+}
+std::pair<Point, Point> Rectangular_trapezoid::getBounds() const {
+    return { bottomLeft, Point(bottomLeft.x+lowerBase_,bottomLeft.y+height_)};
+}
---
+++ balykov.semyon/T4/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <memory>
+
+#include "rectangle.h"
+#include "ellipse.h"
+#include "rectangular_trapezoid.h"
+#include "composite_shape.h"
+
+void printInfo(const std::vector<std::unique_ptr<Shape>>& shapes) {
+    std::cout << std::fixed << std::setprecision(2);
+    for (size_t i = 0; i < shapes.size(); ++i) {
+        const CompositeShape* composite = dynamic_cast<const CompositeShape*>(shapes[i].get());
+        if (composite) {
+            composite->print(std::cout);
+        }
+        else {
+            Point center = shapes[i]->getCenter();
+            std::cout << "[" << shapes[i]->getName() << ", (";
+            std::cout << center.x << ", " << center.y << "), " << shapes[i]->getArea() << "]\n";
+        }
+    }
+    std::cout << "\n";
+}
+
+int main() {
+    std::vector<std::unique_ptr<Shape>> shapes;
+
+    shapes.push_back(std::make_unique<Rectangle>(Point(0, 0), Point(4, 3)));
+    shapes.push_back(std::make_unique<Ellipse>(Point(5, 5), 2, 1.5));
+    shapes.push_back(std::make_unique<Rectangular_trapezoid>(Point(1, 1), 6, 4, 2));
+    shapes.push_back(std::make_unique<Rectangle>(Point(2, 2), Point(5, 4)));
+    shapes.push_back(std::make_unique<Ellipse>(Point(8, 3), 1.5, 1));
+
+    auto composite = std::make_unique<CompositeShape>();
+
+    composite->addShape(std::make_unique<Rectangle>(Point(10, 10), Point(14, 13)));
+    composite->addShape(std::make_unique<Ellipse>(Point(12, 12), 1, 0.5));
+    shapes.push_back(std::move(composite));
+
+    std::cout << "======= before scale ======\n";
+    printInfo(shapes);
+    double factor = 0.0;
+    if (!(std::cin >> factor))
+    {
+        std::cerr << "Error with std::cin\n";
+        return 1;
+    }
+    if (factor <= 0.0)
+    {
+        std::cerr << "Error factor must be positive\n";
+        return 1;
+    }
+    for (size_t i = 0; i < shapes.size(); ++i) {
+        shapes[i]->scale(factor);
+    }
+
+    std::cout << "======= after scale ======\n";
+    printInfo(shapes);
+    return 0;
+}
