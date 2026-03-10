722d9240ac4be93da4f2f2c64dc51bddf5fe8e32
Balykov Semyon <balykov.semen2010@mail.ru>
2026-03-10 16:01:12 +0300

    Final commit for T4

---
+++ balykov.semyon/T4/Rectangle.h
@@ -0,0 +1,19 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+#include <iostream>
+#include <string>
+#include "shape.h"
+class Rectangle : public Shape {
+private:
+    Point bottomLeft;
+    Point topRight;
+public:
+    Rectangle(const Point& bl, const Point& tr);
+    double getArea() const override;
+    Point getCenter() const override;
+    void move(double dx, double dy) override;
+    void scale(double factor) override;
+    std::string getName() const override;
+    std::pair<Point, Point> getBounds() const override;
+};
+#endif
