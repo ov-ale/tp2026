8b5528a064a6a7e7d5a2e74a2f3f4ea1f50d1d6d
vova <129197404+osirust@users.noreply.github.com>
2026-03-10 19:52:18 +0300

    ivanov.vladimir/T4

--- ivanov.vladimir/T4/composite-shape.h
+++ ivanov.vladimir/T4/composite-shape.h
@@ -7,26 +7,25 @@
 
 class CompositeShape : public Shape {
 public:
+    CompositeShape() = default;
+    CompositeShape(const CompositeShape &) = delete;
+    CompositeShape &operator=(const CompositeShape &) = delete;
+    CompositeShape(CompositeShape &&) = default;
+    CompositeShape &operator=(CompositeShape &&) = default;
+    ~CompositeShape() = default;
+
     void addShape(std::shared_ptr<Shape> shape);
-
     double getArea() const override;
-
     Point getCenter() const override;
-
     void move(double dx, double dy) override;
-
     void move(const Point &newCenter) override;
-
     void scale(double factor) override;
-
     std::string getName() const override;
-
     size_t getSize() const;
-
     std::shared_ptr<Shape> getShape(size_t index) const;
 
 private:
-    std::vector<std::shared_ptr<Shape> > shapes_;
+    std::vector<std::shared_ptr<Shape>> shapes_;
 };
 
 #endif
