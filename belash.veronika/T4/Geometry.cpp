0e3fc128a6183918dad660962d7af459049e4555
NiKaaa09 <Veronika_200929@mail.ru>
2026-03-12 00:18:33 +0300

    Add T4 lab files

--- belash.veronika/T4/Geometry.cpp
+++ belash.veronika/T4/Geometry.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
-#include <memory>
-#include <vector>
 #include <iomanip>
+#include <vector>
+#include <memory>
 #include "Shape.hpp"
 #include "Composite_shape.hpp"
 #include "Ellipse.hpp"
@@ -12,8 +12,10 @@ void printInfo(const std::vector<std::unique_ptr<Shape>>& shapes) {
     for (size_t i = 0; i < shapes.size(); ++i) {
         const std::unique_ptr<Shape>& shape = shapes[i];
         Point center = shape->getCenter();
-        std::cout << "[" << shape->getName() << ", ("
-            << std::fixed << std::setprecision(2) << center.x << ", " << center.y << "), "
+
+        std::cout << "[" << shape->getName() << ",("
+            << std::fixed << std::setprecision(2) << center.x << ", "
+            << center.y << "), "
             << std::setprecision(2) << shape->getArea() << "]";
 
         if (shape->getName() == "COMPOSITE") {
@@ -24,8 +26,9 @@ void printInfo(const std::vector<std::unique_ptr<Shape>>& shapes) {
                 for (size_t j = 0; j < subShapes.size(); ++j) {
                     const std::unique_ptr<Shape>& subShape = subShapes[j];
                     Point subCenter = subShape->getCenter();
-                    std::cout << "\n  [" << subShape->getName() << ", ("
-                        << std::fixed << std::setprecision(2) << subCenter.x << ", " << subCenter.y << "), "
+                    std::cout << "\n  [" << subShape->getName() << ",("
+                        << std::fixed << std::setprecision(2) << subCenter.x << ", "
+                        << subCenter.y << "), "
                         << std::setprecision(2) << subShape->getArea() << "]";
                 }
             }
@@ -43,10 +46,10 @@ int main() {
     shapes.push_back(std::make_unique<Rectangle>(Point{ 3, 2 }, Point{ 5, 4 }));
     shapes.push_back(std::make_unique<Ellipse>(Point{ -2, -1 }, 1.0, 0.5));
 
-    auto composite = std::make_unique<CompositeShape>();
+    std::unique_ptr<CompositeShape> composite = std::make_unique<CompositeShape>();
+
     composite->addShape(std::make_unique<Rectangle>(Point{ 1, 2 }, Point{ 3, 5 }));
     composite->addShape(std::make_unique<Ellipse>(Point{ 4, 3 }, 1.0, 2.0));
-
     shapes.push_back(std::move(composite));
 
     std::cout << "BEFORE SCALING:\n";
