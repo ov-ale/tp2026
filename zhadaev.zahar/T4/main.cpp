65aa74e0800750e9163821e2283cd0ab4a3680b8
Zahar <mr.mojastin@gmail.com>
2026-03-18 11:03:47 +0300

    Update main.cpp

--- zhadaev.zahar/T4/main.cpp
+++ zhadaev.zahar/T4/main.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include <iomanip>
 #include "Rectangle.h"
 #include "Square.h"
 #include "Ring.h"
 #include "Composite_shape.h"
+
+void printInfo(const std::vector<std::unique_ptr<Shape>>& shapes)
+{
+    std::cout << std::fixed << std::setprecision(2);
+    for (size_t i = 0; i < shapes.size(); ++i)
+    {
+        shapes[i]->print();
+        std::cout << "\n";
+    }
+}
+
 int main()
 {
-return 0;
+    std::vector<std::unique_ptr<Shape>> shapes;
+    shapes.push_back(std::make_unique<Rectangle>(Point{ 1,1 }, Point{ 4,3 }));
+    shapes.push_back(std::make_unique<Square>(Point{ 2,2 }, 3));
+    shapes.push_back(std::make_unique<Ring>(Point{ 0,0 }, 5, 3));
+    shapes.push_back(std::make_unique<Rectangle>(Point{ 0,0 }, Point{ 2,2 }));
+    shapes.push_back(std::make_unique<Square>(Point{ 3,3 }, 2));
+
+    auto composite = std::make_unique<CompositeShape>();
+    composite->addShape(std::make_unique<Rectangle>(Point{ 10, 10 }, Point{ 14, 13 }));
+    composite->addShape(std::make_unique<Ring>(Point{ 12, 12 }, 1, 0.5));
+    shapes.push_back(std::move(composite));
+    
+    printInfo(shapes);
+
+    double k = 0.0;
+    if (!(std::cin >> k))
+    {
+        std::cerr << "Error with std::cin\n";
+        return 1;
+    }
+    if (k <= 0.0)
+    {
+        std::cerr << "Error k must be positive\n";
+        return 1;
+    }
+    for (size_t i = 0; i < shapes.size(); ++i) {
+        shapes[i]->scale(k);
+    }
+
+    printInfo(shapes);
+
+    return 0;
 }
