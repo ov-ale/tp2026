2d22758529fd20215691901d4659feeb1af37037
Pavel <pavelgavrilov812@gmail.com>
2026-03-19 12:45:35 +0300

    Update point.h

--- gavrilov.pavel/T4/point.h
+++ gavrilov.pavel/T4/point.h
@@ -6,6 +6,18 @@ struct Point {
     double y;
 
     Point(double x = 0, double y = 0) : x(x), y(y) {}
+
+    Point operator+(const Point& other) const {
+        return Point(x + other.x, y + other.y);
+    }
+
+    Point operator-(const Point& other) const {
+        return Point(x - other.x, y - other.y);
+    }
+
+    Point operator*(double factor) const {
+        return Point(x * factor, y * factor);
+    }
 };
 
 #endif
