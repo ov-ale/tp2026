4871c86f0f5eda46204596853bb44c6308f60c2b
ov-ale <o.v.alexandrov@yandex.ru>
2026-03-24 13:52:42 +0300

    Revert "Revert "sidorenko.anastasia/T4""

---
+++ sidorenko.anastasia/T4/rightTrapezoid.cpp
@@ -0,0 +1,52 @@
+#include "rightTrapezoid.h"
+#include <stdexcept>
+
+RightTrapezoid::RightTrapezoid(const Point& bl, double bW, double tW, double h)
+{
+    if (bW <= 0 || tW <= 0 || h <= 0)
+    {
+        throw std::invalid_argument("Uncorrect arguments for right trapezoid\n");
+    }
+    bottomLeft = bl;
+    bottomWidth = bW;
+    topWidth = tW;
+    height = h;
+}
+double RightTrapezoid::getArea() const
+{
+    return ((bottomWidth + topWidth) / 2.0) * height;
+}
+Point RightTrapezoid::getCenter() const
+{
+    double centerX = bottomLeft.x + (bottomWidth + topWidth) / 4.0;
+    double centerY = bottomLeft.y + height / 2.0;
+    return Point(centerX, centerY);
+}
+void RightTrapezoid::move(double dx, double dy)
+{
+    bottomLeft.x += dx;
+    bottomLeft.y += dy;
+}
+void RightTrapezoid::scale(double factor)
+{
+    Point center = getCenter();
+    bottomLeft.x = center.x + (bottomLeft.x - center.x) * factor;
+    bottomLeft.y = center.y + (bottomLeft.y - center.y) * factor;
+    bottomWidth *= factor;
+    topWidth *= factor;
+    height *= factor;
+
+}
+std::string RightTrapezoid::getName() const
+{
+    return "RIGHT TRAPEZOID";
+}
+std::pair<Point, Point> RightTrapezoid::getFrame() const
+{
+    double minX = bottomLeft.x;
+    double maxX = bottomLeft.x + std::max(bottomWidth, topWidth);
+    double minY = bottomLeft.y;
+    double maxY = bottomLeft.y + height;
+
+    return { Point(minX, minY), Point(maxX, maxY) };
+}
---
+++ sidorenko.anastasia/T4/rightTrapezoid.h
@@ -0,0 +1,24 @@
+#ifndef RIGHTTRAPEZOID_H
+#define RIGHTTRAPEZOID_H
+#include "shape.h"
+#include "point.h"
+#include <string>
+
+class RightTrapezoid : public Shape
+{
+public:
+    RightTrapezoid(const Point& bl, double bW, double tW, double h);
+    ~RightTrapezoid() = default;
+    double getArea() const override;
+    Point getCenter() const override;
+    void move(double dx, double dy) override;
+    void scale(double factor) override;
+    std::string getName() const override;
+    std::pair<Point, Point> getFrame() const override;
+private:
+    Point bottomLeft;
+    double bottomWidth;
+    double topWidth;
+    double height;
+};
+#endif
