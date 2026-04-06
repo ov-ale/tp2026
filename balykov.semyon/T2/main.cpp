a73c8d0b111ca54602b648c70559d7f373d508ba
Balykov Semyon <balykov.semen2010@mail.ru>
2026-04-06 19:45:14 +0300

    Fix3 T2

--- balykov.semyon/T2/main.cpp
+++ balykov.semyon/T2/main.cpp
@@ -51,9 +51,10 @@ std::istream& operator>>(std::istream& in,
     LabelIO&& dest);
 std::istream& operator>>(std::istream& in,
     ULLBinIO&& dest);
-std::istream& operator>>(std::istream& in, 
+std::istream& operator>>(std::istream& in,
     ComplexIO&& dest);
-std::istream& operator>>(std::istream& in, StringIO&& dest);
+std::istream& operator>>(std::istream& in,
+    StringIO&& dest);
 std::istream& operator>>(std::istream& in, DataStruct& dest);
 std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
 
