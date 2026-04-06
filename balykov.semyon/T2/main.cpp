636eae426070a93dcc537657d1e15d38ff38af72
Balykov Semyon <balykov.semen2010@mail.ru>
2026-04-06 20:46:09 +0300

    T2 final

--- balykov.semyon/T2/main.cpp
+++ balykov.semyon/T2/main.cpp
@@ -57,7 +57,8 @@ std::istream& operator>>(std::istream& in,
     StringIO&& dest);
 std::istream& operator>>(std::istream& in, 
     DataStruct& dest);
-std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
+std::ostream& operator<<(std::ostream& out, const 
+    DataStruct& dest);
 
 iofmtguard::iofmtguard(std::basic_ios<char>& s) :
     s_(s),
