c0c6f6380663be7cafbaeff47def8f1e18ca2c83
sretenskijila <sretenskijila@gmail.com>
2026-04-14 23:27:01 +0300

    adding a space

--- sretenskiy.ilya/T2/main.cpp
+++ sretenskiy.ilya/T2/main.cpp
@@ -198,7 +198,6 @@ int main() {
     std::vector<DataStruct> data;
     std::copy(std::istream_iterator<DataStruct>(std::cin), std::istream_iterator<DataStruct>(), std::back_inserter(data));
     std::sort(data.begin(), data.end(), compare);
-    std::copy(data.begin(), data.end(),
-        std::ostream_iterator<DataStruct>(std::cout, "\n"));
+    std::copy(data.begin(), data.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
     return 0;
 }
