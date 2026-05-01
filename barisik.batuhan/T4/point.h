9270914ceccdb883739ed6693e27eb86e06f9882
batuhandb <batuhan.deha.barisik@gmail.com>
2026-05-02 00:01:30 +0300

    T4: Add Circle, IsoscelesTrapezoid and CompositeShape implementation

---
+++ barisik.batuhan/T4/point.h
@@ -0,0 +1,12 @@
+#ifndef POINT_H
+#define POINT_H
+
+struct Point {
+    double x = 0.0;
+    double y = 0.0;
+
+    Point() = default;
+    Point(double x_val, double y_val) : x(x_val), y(y_val) {}
+};
+
+#endif
