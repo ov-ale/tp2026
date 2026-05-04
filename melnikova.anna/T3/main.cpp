c2e9dd39b9d442947ed3c8fb3d293f41e2457ff8
Melnikova Anna <melnikova2.am@edu.spbstu.ru>
2026-05-04 23:25:31 +0300

    fix content(

--- melnikova.anna/T3/main.cpp
+++ melnikova.anna/T3/main.cpp
@@ -239,7 +239,11 @@ int main(int argc, char* argv[]) {
                     if (n < 3) throw std::invalid_argument("n < 3");
                     res = std::accumulate(polys.begin(), polys.end(), 0.0, std::bind(addAreaIfNum, _1, _2, n));
                 }
-                catch (...) { std::cout << "<INVALID COMMAND>\n"; std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); continue; }
+                catch (...) {
+                    std::cout << "<INVALID COMMAND>\n";
+                    std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    continue;
+                }
             }
             std::cout << std::fixed << std::setprecision(1) << res << "\n";
         }
@@ -277,13 +281,20 @@ int main(int argc, char* argv[]) {
                     if (n < 3) throw std::invalid_argument("n < 3");
                     std::cout << std::count_if(polys.begin(), polys.end(), std::bind(hasNumVertices, _1, n)) << "\n";
                 }
-                catch (...) { std::cout << "<INVALID COMMAND>\n"; std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); continue; }
+                catch (...) { 
+                    std::cout << "<INVALID COMMAND>\n";
+                    std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    continue;
+                }
             }
         }
         else if (cmd == "RMECHO") {
             Polygon target;
-            if (!(std::cin >> target)) { std::cout << "<INVALID COMMAND>\n"; std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); continue; }
-
+            if (!(std::cin >> target)) {
+                std::cout << "<INVALID COMMAND>\n";
+                std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                continue;
+            }
             auto it = std::unique(polys.begin(), polys.end(), std::bind(checkDuplicateTarget, _1, _2, target));
             int removed = std::distance(it, polys.end());
             polys.erase(it, polys.end());
@@ -291,8 +302,11 @@ int main(int argc, char* argv[]) {
         }
         else if (cmd == "INTERSECTIONS") {
             Polygon target;
-            if (!(std::cin >> target)) { std::cout << "<INVALID COMMAND>\n"; std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); continue; }
-
+            if (!(std::cin >> target)) {
+                std::cout << "<INVALID COMMAND>\n";
+                std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                continue;
+            }
             int currentMaxX = std::max(globalMaxX, getPolygonMaxX(target));
             std::cout << std::count_if(polys.begin(), polys.end(), std::bind(polygonsIntersect, _1, target, currentMaxX)) << "\n";
         }
