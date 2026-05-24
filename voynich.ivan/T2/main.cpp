cbf4e46119ef7ec39a9e28e7303b0829c059b680
Ivan Voynich <bandgamin719@gmail.com>
2026-05-25 02:03:44 +0300

    delete ws

--- voynich.ivan/T2/main.cpp
+++ voynich.ivan/T2/main.cpp
@@ -6,23 +6,22 @@
 
 int main() {
     std::vector<DataStruct> data;
-    
+
 
     std::copy(
         std::istream_iterator<DataStruct>(std::cin),
         std::istream_iterator<DataStruct>(),
         std::back_inserter(data)
     );
-    
+
     std::sort(data.begin(), data.end(), compareDataStruct);
-    
-    
+
     std::copy(
         data.begin(),
         data.end(),
         std::ostream_iterator<DataStruct>(std::cout, "\n")
     );
-    
+
     return 0;
 }
 
